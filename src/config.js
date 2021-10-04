f8981688dd67b0f3b2cfebd2d44658fa9e7212fe
Victor Hernandez <victor-2027@hotmail.com>
2021-10-03 22:35:45 -0500

    xd

--- src/config.js
+++ src/config.js
@@ -9,7 +9,8 @@ llaves.GOOGLE = {
 // llaves.API = "http://192.168.0.105:3000";
 // llaves.API = "http://192.168.0.105:4000";
 // llaves.API = "http://localhost:4000";
-llaves.API = 'https://prueba-naztec.herokuapp.com'
+// llaves.API = 'https://prueba-naztec.herokuapp.com'
+llaves.API = 'https://famircentro.com'
 
 llaves.DATABASE_NAME = "estudioo_famir";
 llaves.DATABASE_HOST = "estudioochoamaldonado.com";
